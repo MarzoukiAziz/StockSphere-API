ac0ffd2d4ed4ce48638ab57b04749e88e968184c
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2022-06-16 15:09:52 +0100

    feat: MvtStk services

--- src/main/java/com/bouali/gestiondestock/validator/MvtStkValidator.java
+++ src/main/java/com/bouali/gestiondestock/validator/MvtStkValidator.java
@@ -1,14 +1,14 @@
 package com.bouali.gestiondestock.validator;
 
-import com.bouali.gestiondestock.dto.CommandeClientDto;
+import com.bouali.gestiondestock.dto.MvtStkDto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommandeClientValidator {
+public class MvtStkValidator {
 
-
-  public static List<String> validate(CommandeClientDto dto) {
+  public static List<String> validate(MvtStkDto mvtStkDto) {
     List<String> errors = new ArrayList<>();
+
 
     return errors;
   }
