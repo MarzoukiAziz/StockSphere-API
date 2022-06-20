6252afc0162a8cc38132070c7c9c7ea2236f1f38
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2022-06-20 21:37:25 +0100

    feat: Authentification and users management

--- src/main/java/com/bouali/gestiondestock/validator/CommandeClientValidator.java
+++ src/main/java/com/bouali/gestiondestock/validator/CommandeClientValidator.java
@@ -1,25 +1,34 @@
 package com.bouali.gestiondestock.validator;
 
-import com.bouali.gestiondestock.dto.VentesDto;
+import com.bouali.gestiondestock.dto.CommandeClientDto;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.util.StringUtils;
 
-public class VentesValidator {
+public class CommandeClientValidator {
 
-  public static List<String> validate(VentesDto dto) {
+
+  public static List<String> validate(CommandeClientDto dto) {
     List<String> errors = new ArrayList<>();
     if (dto == null) {
       errors.add("Veuillez renseigner le code de la commande");
       errors.add("Veuillez renseigner la date de la commande");
+      errors.add("Veuillez renseigner l'etat de la commande");
+      errors.add("Veuillez renseigner le client");
       return errors;
     }
 
     if (!StringUtils.hasLength(dto.getCode())) {
       errors.add("Veuillez renseigner le code de la commande");
     }
-    if (dto.getDateVente() == null) {
+    if (dto.getDateCommande() == null) {
       errors.add("Veuillez renseigner la date de la commande");
+    }
+    if (!StringUtils.hasLength(dto.getEtatCommande().toString())) {
+      errors.add("Veuillez renseigner l'etat de la commande");
+    }
+    if (dto.getClient() == null || dto.getClient().getId() == null) {
+      errors.add("Veuillez renseigner le client");
     }
 
     return errors;
